8085442fd262215fafdecc1353eace01ac6bdf7f
Jordan <jordan3steele@gmail.com>
2016-03-26 14:20:37 -0700

    Initial Commit

---
+++ WormShootTests/WormShootTests.swift
@@ -0,0 +1,36 @@
+//
+//  WormShootTests.swift
+//  WormShootTests
+//
+//  Created by Jordan Steele on 2016-03-26.
+//  Copyright © 2016 Jordan Steele. All rights reserved.
+//
+
+import XCTest
+@testable import WormShoot
+
+class WormShootTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
